880bc8ea4de5b38b1f91f4f6137caa805bbb6d1e
DariaShirgina <d.d.shirgina@gmail.com>
2023-07-30 23:46:28 +0300

    home_work5_4

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, register, login, advertisement_post, profile
 
 urlpatterns = [
-    path('', index),
+    path('', index, name = '/'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+
 ]
