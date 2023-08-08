45c9ca90d191f6ee4552fb752db24ebf39b923af
DariaShirgina <d.d.shirgina@gmail.com>
2023-08-08 17:44:30 +0300

    home_work7_4

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -80,6 +80,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # ,
+    #
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':  'db.postgresql',
+    #     'USER': 'user',
+    #     'PASSWORD': 'password',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432'
+    # }
 }
 
 
