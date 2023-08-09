b49bcf2df36af102f9ea563761b72cfabc8f488d
DariaShirgina <d.d.shirgina@gmail.com>
2023-08-09 19:11:40 +0300

    home_work8_4

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement1(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=128)
@@ -13,3 +15,19 @@ class Advertisement1(models.Model):
 
     def __str__(self):
         return f"Advertisement1(id={self.id}, title={self.title}, price={self.price})"
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:green; font-weight:bold"> Сегодня в {} </span>', created_time)
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            update_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:blue; font-weight:bold"> Сегодня в {} </span>', update_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
